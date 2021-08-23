f3c1d3a23e4b2b00c94a3366815b52cee58013e6
Wyz3r <leonelislasglez@gmail.com>
2021-08-23 18:00:06 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TaskModule } from './task/task.module';
+
+
+
+@Module({
+  imports: [TaskModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/task/task.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+import { TaskController } from './task.controller';
+
+
+@Module({
+  controllers: [TaskController]
+})
+export class TaskModule {}
